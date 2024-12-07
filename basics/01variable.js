6cf305186a50eb44fb43a96e49be7afc4824c793
spy2022 <98205122+spy2022@users.noreply.github.com>
2024-12-07 16:14:14 +0530

    Create 01variable.js

---
+++ basics/01variable.js
@@ -0,0 +1,10 @@
+const accountId=234
+let accountname="nitin"
+var accountpassword="234"
+accountcity="jaipur"
+
+// accountId=762
+// console.log(accountcity);
+console.log(accountId);
+
+console.table([accountId,accountname,accountpassword,accountcity]);
